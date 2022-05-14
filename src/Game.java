3558602015d6ac7580b432a2edd5e1d9f0b49b16
abounk <a_bunk911@hotmail.com>
2022-05-14 16:29:56 +0700

    initial commit

---
+++ src/Game.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class Game extends JFrame {
+    private Thread gameThread;
+//    private WorldPanel worldPanel;
+
+    class WorldPanel extends JPanel {
+        public static final int PIXEL_SIZE = 20;
+
+        WorldPanel() {
+
+        }
+    }
+
+}
